11d012b33cc2b25ca60a78b3be2f243baece0169
Niclas | Ravish <43492760+NequZ@users.noreply.github.com>
2022-09-27 09:00:47 +0200

    added some icons for later usage; edit query for different icons in Comment Sections

--- admin/editticket.php
+++ admin/editticket.php
@@ -48,9 +48,6 @@ echo "<h2>Ticket #" . $row['id'] . "</h2>";
                                     <tr data-status="pagado">
                                         <td>
                                             <div class="media">
-                                                <a href="#" class="pull-left">
-                                                    <img src="https://bootdey.com/img/Content/user_1.jpg" class="media-photo">
-                                                </a>
                                                 <div class="media-body">
                                                     <span class="media-meta pull-right">Ticket ID: <?php echo $row['id']; ?></span>
                                                     <h4 class="title">
@@ -88,8 +85,19 @@ echo "<h2>Ticket #" . $row['id'] . "</h2>";
                                         echo "<tr data-status='pagado'>";
                                         echo "<td>";
                                         echo "<div class='media'>";
-                                        echo "<a href='#' class='pull-left'>";
-                                        echo "<img src='https://bootdey.com/img/Content/user_1.jpg' class='media-photo'>";
+                                        if ($row['creator'] == "Staff Member") {  // Thats the Avatar for the Staff Team. You can change it to your own. At the Moment every Staff Member has the same Picture. You can change it if you want.
+                                            echo "<a href='#' class='pull-left'>";
+                                            echo "<img src='../img/staffmember.png' class='media-photo'>";
+                                            echo "</a>";
+                                        } elseif ($row['creator'] == "User") {  // Thats the Avatar of the User who created the Ticket. You can change it to your own. Reminder: The User cant change the Avatar. And he also cant see it on his Interface.
+                                            echo "<a href='#' class='pull-left'>";
+                                            echo "<img src='../img/user.png' class='media-photo'>";
+                                            echo "</a>";
+                                            } else {  // Thats the Avatar for the System. When the System creates a comment. Like : Ticket Closed. You can change it to your own Avatar.
+                                            echo "<a href='#' class='pull-left'>";
+                                            echo "<img src='../img/systemavatar.png' class='media-photo'>";
+                                            echo "</a>";
+                                        }
                                         echo "</a>";
                                         echo "<div class='media-body'>";
                                         echo "<span class='media-meta pull-right'>Comment ID: " . $row['id'] . "</span>";
@@ -97,6 +105,8 @@ echo "<h2>Ticket #" . $row['id'] . "</h2>";
                                         echo $row['message'];
                                         echo "<br>";
                                         echo "<span class='pull-right pagado'>" . $row['created'] . "</span>";
+                                        echo "<br>";
+                                        echo "<br><span class='pull-right pagado'>By: " . $row['creator'] . "</span>";
                                         echo "</div>";
                                         echo "</td>";
                                         echo "</tr>";
